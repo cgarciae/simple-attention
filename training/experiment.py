07291645b80c77aa177277e4e7d7f0b8baf5bb26
Cristian Garcia <cgarcia.e88@gmail.com>
2020-01-20 12:17:35 -0500

    save

--- training/experiment.py
+++ training/experiment.py
@@ -1,23 +1,27 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 import typer
 from plotly import express as px
 from plotly import graph_objs as go
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import numpy as np
+
+from .tf_keras_transformer import MultiHeadSelfAttention
 
 
 def main(
     data_path: Path = typer.Option(...),
     n_classes: int = typer.Option(...),
     memory_size: int = 8,
-    n_layers: int = 0,
+    n_layers: int = 1,
     n_neurons: int = 16,
-    k: float = 1.0,
+    n_heads: int = 4,
     epochs: int = 400,
+    batch_size: int = 16,
+    lr: float = 0.001,
     viz: bool = False,
 ) -> None:
 
@@ -27,91 +31,117 @@ def main(
     df_train = pd.read_csv(train_path, names=["x1", "x2", "label"])
     df_test = pd.read_csv(test_path, names=["x1", "x2", "label"])
 
-    X_train = df_train[["x1", "x2"]].to_numpy()
+    X_train = df_train[["x1", "x2"]].to_numpy().astype(np.float32)
     y_train = df_train["label"].to_numpy().astype(int)
 
-    X_test = df_test[["x1", "x2"]].to_numpy()
+    X_test = df_test[["x1", "x2"]].to_numpy().astype(np.float32)
     y_test = df_test["label"].to_numpy().astype(int)
 
     transform = StandardScaler()
 
-    X_train = transform.fit_transform(X_train)
-    X_test = transform.transform(X_test)
+    # X_train = transform.fit_transform(X_train)
+    # X_test = transform.transform(X_test)
+
+    idx = np.random.choice(400, 70)
 
     model = MyModel(
-        (2,),
+        x=X_train[idx],
+        y=y_train[idx],
         memory_size=memory_size,
+        memory_output_size=2,
         n_labels=n_classes,
-        k=k,
+        n_heads=n_heads,
         n_layers=n_layers,
         n_neurons=n_neurons,
     )
-
+    model(X_train[:2])
     model.summary()
 
     model.compile(
         optimizer=tf.optimizers.Adam(0.001),
-        loss=tf.losses.sparse_categorical_crossentropy,
-        metrics=[tf.metrics.SparseCategoricalAccuracy()],
+        loss=tf.losses.BinaryCrossentropy(),
+        metrics=[tf.metrics.BinaryAccuracy()],
     )
 
     model.fit(
         x=X_train,
         y=y_train,
-        batch_size=32,
+        batch_size=4,
         epochs=epochs,
         validation_data=(X_test, y_test),
     )
-
-    simple_memory = model.layers[1]
+    model.summary()
 
     if viz:
-        fig = px.scatter(None, X_train[:, 0], X_train[:, 1], color=y_train)
-
-        memory = simple_memory.memory.kernel.numpy()
+        fig = px.scatter(x=X_train[:, 0], y=X_train[:, 1], color=y_train)
 
         xx, yy, zz = decision_boundaries(X_train, model)
         xx = xx[0]
         yy = yy[:, 0]
 
+        print(xx)
+
         fig.add_trace(go.Contour(x=xx, y=yy, z=zz, opacity=0.5))
-        fig.add_trace(
-            go.Scatter(
-                x=memory[:, 0],
-                y=memory[:, 1],
-                mode="markers",
-                marker=dict(color="black", size=10),
-            )
-        )
 
         fig.show()
 
 
-def MyModel(input_shape, memory_size, n_labels, k=1, n_layers=0, n_neurons=16):
+class MyModel(tf.keras.Model):
+    def __init__(
+        self,
+        x,
+        y,
+        memory_size,
+        memory_output_size,
+        n_labels,
+        n_heads=4,
+        n_layers=1,
+        n_neurons=16,
+    ):
+        super().__init__()
 
-    inputs = tf.keras.Input(shape=input_shape)
+        self.x = tf.constant(x)[None]
+        self.y = tf.constant(y, dtype=tf.float32)[None, :, None]
 
-    net = SimpleMemory(memory_size, k=k)(inputs)
+        self.x_embeddings = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(n_neurons, activation="relu")
+                for _ in range(n_layers - 1)
+            ]
+            + [tf.keras.layers.Dense(n_neurons)]
+        )
+        self.attention = tf.keras.layers.Attention(use_scale=True)
 
-    for i in range(n_layers):
-        net = tf.keras.layers.Dense(n_neurons, activation="relu")(net)
+        # self.dense_out = tf.keras.layers.Dense(n_labels, activation="softmax")
 
-    net = tf.keras.layers.Dense(n_labels, activation="softmax")(net)
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
 
-    model = tf.keras.Model(inputs=inputs, outputs=net)
+        x = inputs[:, None, :]
+        ds_x = tf.tile(self.x, [batch_size, 1, 1])
+        ds_y = tf.tile(self.y, [batch_size, 1, 1])
 
-    return model
+        q = self.x_embeddings(x)
+        k = self.x_embeddings(ds_x)
+        v = ds_y
+
+        print(q.shape, k.shape, v.shape)
+
+        net = self.attention([q, v, k])[:, 0]
+        print(net.shape)
+
+        # net = self.dense_out(net)
+        # print(net.shape)
+
+        return net
 
 
 class SimpleMemory(tf.keras.Model):
-    def __init__(self, memory_size, k=1):
+    def __init__(self, memory_size, output_size, k=1):
         super().__init__()
-        self.memory_size = memory_size
         self.k = k
-
-    def build(self, input_shape):
-        self.dense = tf.keras.layers.Dense(self.memory_size)
-        self.memory = tf.keras.layers.Dense(input_shape[-1], use_bias=False)
+        self.dense = tf.keras.layers.Dense(memory_size)
+        self.memory = tf.keras.layers.Dense(output_size, use_bias=False)
 
     def call(self, inputs):
 
@@ -122,27 +152,25 @@ class SimpleMemory(tf.keras.Model):
         return net
 
 
-def decision_boundaries(X, model, n=10):
+def decision_boundaries(X, model, n=20):
 
     # Plot the decision boundary. For that, we will assign a color to each
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    x_min, x_max = X[:, 0].min() - 2, X[:, 0].max() + 2
+    y_min, y_max = X[:, 1].min() - 2, X[:, 1].max() + 2
+
+    print(x_min, x_max)
+    print(y_min, y_max)
+
     hx = (x_max - x_min) / n
     hy = (y_max - y_min) / n
     xx, yy = np.meshgrid(np.arange(x_min, x_max, hx), np.arange(y_min, y_max, hy))
 
     # Obtain labels for each point in mesh using the model.
-    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    points = np.c_[xx.ravel(), yy.ravel()]
+    Z = model.predict(points)
+    Z = (Z > 0.5).astype(np.int32)
 
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
-
-    preds = model.predict(np.c_[xx.ravel(), yy.ravel()])
-
-    preds = np.argmax(preds, axis=-1)
-
-    zz = preds.reshape(xx.shape)
+    zz = Z.reshape(xx.shape)
 
     return xx, yy, zz
 
--- training/experiment.sh
+++ training/experiment.sh
@@ -1,11 +1,13 @@
 
-DATA_PATH="data/spirals"
-N_CLASSES=2
-
-export CUDA_VISIBLE_DEVICES=""
+# export CUDA_VISIBLE_DEVICES=""
 
 
 python -m training.experiment \
-    --data-path ${DATA_PATH} \
-    --n-classes ${N_CLASSES} \
+    --data-path "data/spirals" \
+    --n-classes 2 \
+    --batch-size 4 \
+    --lr 0.0002 \
+    --epochs 300 \
+    --n-layers 2 \
+    --n-neurons 64 \
     "$@"
