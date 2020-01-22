56f2df864cfec5c75fb0dade3eb14566809dac83
Cristian Garcia <cgarcia.e88@gmail.com>
2020-01-22 13:07:19 -0500

    save

--- training/experiment.py
+++ training/experiment.py
@@ -39,12 +39,12 @@ def main(
 
     transform = StandardScaler()
 
-    # X_train = transform.fit_transform(X_train)
-    # X_test = transform.transform(X_test)
+    X_train = transform.fit_transform(X_train)
+    X_test = transform.transform(X_test)
 
     idx = np.random.choice(400, 70)
 
-    model = MyModel(
+    model = SimpleMemory(
         x=X_train[idx],
         y=y_train[idx],
         memory_size=memory_size,
@@ -58,7 +58,7 @@ def main(
     model.summary()
 
     model.compile(
-        optimizer=tf.optimizers.Adam(0.001),
+        optimizer=tf.optimizers.Adam(lr=lr),
         loss=tf.losses.BinaryCrossentropy(),
         metrics=[tf.metrics.BinaryAccuracy()],
     )
@@ -75,6 +75,8 @@ def main(
     if viz:
         fig = px.scatter(x=X_train[:, 0], y=X_train[:, 1], color=y_train)
 
+        fig.update_traces(mode="markers", marker_line_width=2)
+
         xx, yy, zz = decision_boundaries(X_train, model)
         xx = xx[0]
         yy = yy[:, 0]
@@ -86,7 +88,7 @@ def main(
         fig.show()
 
 
-class MyModel(tf.keras.Model):
+class SimpleMemory(tf.keras.Model):
     def __init__(
         self,
         x,
@@ -100,54 +102,35 @@ class MyModel(tf.keras.Model):
     ):
         super().__init__()
 
-        self.x = tf.constant(x)[None]
-        self.y = tf.constant(y, dtype=tf.float32)[None, :, None]
+        self.x = x
+        self.y = y
 
-        self.x_embeddings = tf.keras.Sequential(
+        self.f = tf.keras.Sequential(
             [
-                tf.keras.layers.Dense(n_neurons, activation="relu")
-                for _ in range(n_layers - 1)
+                tf.keras.layers.Dense(
+                    n_neurons, activation="relu" if i < n_layers - 1 else None
+                )
+                for i in range(n_layers)
             ]
-            + [tf.keras.layers.Dense(n_neurons)]
         )
         self.attention = tf.keras.layers.Attention(use_scale=True)
-
-        # self.dense_out = tf.keras.layers.Dense(n_labels, activation="softmax")
 
     def call(self, inputs):
         batch_size = tf.shape(inputs)[0]
 
-        x = inputs[:, None, :]
-        ds_x = tf.tile(self.x, [batch_size, 1, 1])
-        ds_y = tf.tile(self.y, [batch_size, 1, 1])
+        xq = inputs[:, None, :]
 
-        q = self.x_embeddings(x)
-        k = self.x_embeddings(ds_x)
-        v = ds_y
+        x = tf.constant(self.x)[None]
+        x = tf.tile(x, [batch_size, 1, 1])
 
-        print(q.shape, k.shape, v.shape)
+        y = tf.constant(self.y, dtype=tf.float32)[None, :, None]
+        y = tf.tile(y, [batch_size, 1, 1])
+
+        q = self.f(xq)
+        k = self.f(x)
+        v = y
 
         net = self.attention([q, v, k])[:, 0]
-        print(net.shape)
-
-        # net = self.dense_out(net)
-        # print(net.shape)
-
-        return net
-
-
-class SimpleMemory(tf.keras.Model):
-    def __init__(self, memory_size, output_size, k=1):
-        super().__init__()
-        self.k = k
-        self.dense = tf.keras.layers.Dense(memory_size)
-        self.memory = tf.keras.layers.Dense(output_size, use_bias=False)
-
-    def call(self, inputs):
-
-        net = self.dense(inputs)
-        net = tf.nn.softmax(net * self.k)
-        net = self.memory(net)
 
         return net
 
@@ -155,18 +138,21 @@ class SimpleMemory(tf.keras.Model):
 def decision_boundaries(X, model, n=20):
 
     # Plot the decision boundary. For that, we will assign a color to each
-    x_min, x_max = X[:, 0].min() - 2, X[:, 0].max() + 2
-    y_min, y_max = X[:, 1].min() - 2, X[:, 1].max() + 2
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 
     print(x_min, x_max)
     print(y_min, y_max)
 
     hx = (x_max - x_min) / n
     hy = (y_max - y_min) / n
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, hx), np.arange(y_min, y_max, hy))
+    xx, yy = np.meshgrid(
+        np.arange(x_min, x_max + hx, hx), np.arange(y_min, y_max + hy, hy)
+    )
 
     # Obtain labels for each point in mesh using the model.
-    points = np.c_[xx.ravel(), yy.ravel()]
+    points = np.stack([xx.ravel(), yy.ravel()], axis=1)
+
     Z = model.predict(points)
     Z = (Z > 0.5).astype(np.int32)
 
--- training/experiment.sh
+++ training/experiment.sh
@@ -5,9 +5,9 @@
 python -m training.experiment \
     --data-path "data/spirals" \
     --n-classes 2 \
-    --batch-size 4 \
+    --batch-size 8 \
     --lr 0.0002 \
-    --epochs 300 \
+    --epochs 500 \
     --n-layers 2 \
     --n-neurons 64 \
     "$@"
